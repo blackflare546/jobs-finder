c5841193f5702cac447ec1b8df0ca177194266f2
Glenn Mark <gflores@hyperstacksinc.com>
2023-07-27 15:28:01 +0800

    feat: Add Bottom Navigations

--- src/scenes/Entry/hooks/use-signin.hooks.ts
+++ src/scenes/Entry/hooks/use-signin.hooks.ts
@@ -2,8 +2,11 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup/dist/yup";
 import { loginValidateSchema } from "../validations/login.validate";
 import { ISigninForm } from "../../../stores/interface/signin-form.interface";
+import { useNavigation } from "@react-navigation/native";
 
 export const useSignin = () => {
+  const navigation = useNavigation();
+
   const defaultValues = {
     email: "",
     password: "",
@@ -22,6 +25,8 @@ export const useSignin = () => {
   const onSubmit = (formData: ISigninForm) => {
     if (formData) {
       // Navigate to Main Screen
+      console.log(formData);
+      navigation.navigate("Main", { screen: "Profile Bottom Tab" });
     }
   };
 
--- src/types/navigation-param.type.tsx
+++ src/types/navigation-param.type.tsx
@@ -2,14 +2,36 @@ import { NavigatorScreenParams } from "@react-navigation/native";
 
 declare global {
   namespace ReactNavigation {
-    interface RootParamList extends RootStackParamList, EntryStackParamList {}
+    interface RootParamList
+      extends RootStackParamList,
+        EntryStackParamList,
+        ProfileStackParamList,
+        JobsStackParamList {}
   }
 }
 export type RootStackParamList = {
   Entry: NavigatorScreenParams<EntryStackParamList> | undefined;
+  Main: NavigatorScreenParams<MainBottomTabParamList> | undefined;
 };
 
 export type EntryStackParamList = {
   Landing: undefined;
   Signin: undefined;
+};
+
+export type MainBottomTabParamList = {
+  "Profile Bottom Tab":
+    | NavigatorScreenParams<ProfileStackParamList>
+    | undefined;
+  "Jobs Bottom Tab": NavigatorScreenParams<JobsStackParamList> | undefined;
+};
+
+//DASHBOARD
+export type ProfileStackParamList = {
+  Profile: undefined;
+};
+
+//DASHBOARD
+export type JobsStackParamList = {
+  Jobs: undefined;
 };

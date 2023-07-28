1ca69176fedaedc3d34924e1ad6d8759e15d06e3
Glenn Mark <gflores@hyperstacksinc.com>
2023-07-28 11:46:47 +0800

    style: add styled components to JobsScreen

--- src/scenes/Main/screens/jobs.screen.tsx
+++ src/scenes/Main/screens/jobs.screen.tsx
@@ -1,42 +1,7 @@
 import { View, Text, StyleSheet, Image, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useJobs } from "../hooks/use-jobs.hooks";
-
-const JobPosts: React.FC<JobPostsProps> = ({ item }) => {
-  return (
-    <View>
-      <Text>{item.job_title}</Text>
-    </View>
-  );
-};
-
-export default () => {
-  const { data, isLoading } = useJobs();
-
-  return (
-    <SafeAreaView
-      style={{
-        padding: 15,
-        flexDirection: "row",
-      }}
-    >
-      <View>
-        {isLoading ? (
-          <Text>Loading...</Text>
-        ) : data ? (
-          <FlatList
-            data={data.data}
-            renderItem={({ item }) => <JobPosts item={item} />}
-            keyExtractor={(item, index) => index.toString()}
-            numColumns={2}
-          />
-        ) : (
-          <Text>Whoops No Data Available</Text>
-        )}
-      </View>
-    </SafeAreaView>
-  );
-};
+import { CompanyInfo, Container, JobTitle } from "../styles/jobs.styled";
 
 interface JobPostItem {
   employer_name: string;
@@ -49,3 +14,37 @@ interface JobPostItem {
 interface JobPostsProps {
   item: JobPostItem;
 }
+
+const JobPosts: React.FC<JobPostsProps> = ({ item }) => {
+  return (
+    <Container>
+      <JobTitle>{item.job_title}</JobTitle>
+      <CompanyInfo>
+        <Text>{item.employer_name} | </Text>
+        <Text>{item.job_country}</Text>
+      </CompanyInfo>
+    </Container>
+  );
+};
+
+export default () => {
+  const { data, isLoading } = useJobs();
+
+  return (
+    <SafeAreaView style={{ flex: 1, padding: 15, backgroundColor: "#E5E5E5" }}>
+      <View>
+        {isLoading ? (
+          <Text>Loading...</Text>
+        ) : data ? (
+          <FlatList
+            data={data.data}
+            renderItem={({ item }) => <JobPosts item={item} />}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        ) : (
+          <Text>Whoops No Data Available</Text>
+        )}
+      </View>
+    </SafeAreaView>
+  );
+};

f137036da44c008e5bb02ee3519f2699044a0120
Glenn Mark <gflores@hyperstacksinc.com>
2023-07-28 11:58:58 +0800

    style: add styled components to SigninScreen

--- src/scenes/Entry/screens/signin.screen.tsx
+++ src/scenes/Entry/screens/signin.screen.tsx
@@ -7,58 +7,20 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useSignin } from "../hooks/use-signin.hooks";
-import { Controller, useForm } from "react-hook-form";
-
-const styles = StyleSheet.create({
-  Layout: {
-    justifyContent: "space-evenly",
-    alignItems: "center",
-    height: "100%",
-  },
-  Forms: {
-    gap: 25,
-  },
-  Label: {
-    color: "#0D0140",
-    fontSize: 12,
-    fontWeight: "bold",
-    marginBottom: 10,
-  },
-  Inputs: {
-    backgroundColor: "#fff",
-    height: 50,
-    width: 300,
-    borderRadius: 10,
-    padding: 15,
-  },
-  LoginButton: {
-    marginTop: 20,
-    backgroundColor: "#130160",
-    height: 50,
-    borderRadius: 6,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  LoginText: {
-    color: "#fff",
-    fontSize: 14,
-    fontWeight: "bold",
-  },
-});
+import { Controller } from "react-hook-form";
+import { Forms, Label, Layout, Welcome } from "../styles/signin.styled";
 
 export default () => {
   const { control, handleSubmit, isValid, onSubmit, errors } = useSignin();
 
   return (
     <SafeAreaView>
-      <View style={styles.Layout}>
-        <Text style={{ color: "#0D0140", fontSize: 30, fontWeight: "bold" }}>
-          Welcome Back
-        </Text>
+      <Layout>
+        <Welcome>Welcome Back</Welcome>
 
-        <View style={styles.Forms}>
+        <Forms>
           <View>
-            <Text style={styles.Label}>Email</Text>
+            <Label>Email</Label>
             <Controller
               control={control}
               rules={{
@@ -82,7 +44,7 @@ export default () => {
           </View>
 
           <View>
-            <Text style={styles.Label}>Password</Text>
+            <Label>Password</Label>
 
             <Controller
               control={control}
@@ -112,8 +74,31 @@ export default () => {
           >
             <Text style={styles.LoginText}>Login</Text>
           </TouchableOpacity>
-        </View>
-      </View>
+        </Forms>
+      </Layout>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  Inputs: {
+    backgroundColor: "#fff",
+    height: 50,
+    width: 300,
+    borderRadius: 10,
+    padding: 15,
+  },
+  LoginButton: {
+    marginTop: 20,
+    backgroundColor: "#130160",
+    height: 50,
+    borderRadius: 6,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  LoginText: {
+    color: "#fff",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+});

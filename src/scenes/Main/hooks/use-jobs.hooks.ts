be488a5a20e8dfb64e699f49e8d4a148c34b6610
Glenn Mark <gflores@hyperstacksinc.com>
2023-07-28 16:09:08 +0800

    fix: refactor usejob hooks

--- src/scenes/Main/hooks/use-jobs.hooks.ts
+++ src/scenes/Main/hooks/use-jobs.hooks.ts
@@ -1,26 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const options = {
-  method: "GET",
-  url: "https://jsearch.p.rapidapi.com/search",
-  params: {
-    query: "Python developer in Texas, USA",
-    page: "1",
-    num_pages: "1",
-  },
-  headers: {
-    "X-RapidAPI-Key": "7a6146aa71msh560f814062e4c05p12b843jsnc89fffc7e1cd",
-    "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
-  },
-};
-
-const getAllJobs = async () => {
-  const response = await axios.request(options);
-  return response.data;
-};
-
 export const useJobs = () => {
+  const options = {
+    method: "GET",
+    url: "https://jsearch.p.rapidapi.com/search",
+    params: {
+      query: "Python developer in Texas, USA",
+      page: "1",
+      num_pages: "1",
+    },
+    headers: {
+      "X-RapidAPI-Key": "7a6146aa71msh560f814062e4c05p12b843jsnc89fffc7e1cd",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+  };
+
+  const getAllJobs = async () => {
+    const response = await axios.request(options);
+    return response.data;
+  };
+
   const { isLoading, data } = useQuery(["allJobs"], getAllJobs);
   return { data, isLoading };
 };
